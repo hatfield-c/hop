970a4d9a5b2ab711f843866c721b5f33be93e3f8
Cody Hatfield <hatfield.coders@gmail.com>
2020-10-12 13:14:54 -0400

    [HOP-1] Initial commit

---
+++ Assets/Scripts/GoalZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Spin spin = other.gameObject.GetComponent<Spin>();
+
+        if(spin == null) {
+            return;
+        }
+
+        Debug.Log("Success!");
+        spin.Reset();
+    }
+}
