471c837c2eab193938f0247c66966fc5ee8e1e72
Cody Hatfield <hatfield.coders@gmail.com>
2020-10-26 04:45:47 -0400

    [HOP-15] Initial stage one set up

--- Assets/Scripts/Level/AnimatedObject.cs
+++ Assets/Scripts/Level/AnimatedObject.cs
@@ -10,18 +10,38 @@ public class AnimatedObject : AbstractResettable
     public float animationTime = 1f;
     public LeanTweenType easeType = LeanTweenType.linear;
     public bool orientToPath = true;
+    public bool animate = true;
 
     protected Vector3[] path;
     protected int tweenId = -1;
 
     public override void ResetObject() {
+        if(this.pathList.Count < 4 || !this.animate) {
+            return;
+        }
+
         if(this.tweenId > -1) {
             LeanTween.cancel(this.tweenId);
         }
 
+        this.ActivateAnimation();
+    }
+
+    public void StopAnimation() {
+        this.animate = false;
+        LeanTween.cancel(this.tweenId);
+        this.tweenId = -1;
+    }
+
+    public void StartAnimation() {
+        this.animate = true;
+        this.ActivateAnimation();
+    }
+
+    protected void ActivateAnimation() {
         this.tweenId = LeanTween.moveSpline(
-            this.gameObject, 
-            this.path, 
+            this.gameObject,
+            this.path,
             this.animationTime
         ).setLoopPingPong().setOrientToPath(this.orientToPath).setEase(this.easeType).id;
     }
